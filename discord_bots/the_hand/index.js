d76536583cfc653cdb54713b14e6f6836af53003
Noland Seigler <nolandseigler@gmail.com>
2019-11-23 20:21:42 -0600

    bot can kick members. sends gif from giphy when action is completed. has error message capability. still requires testing.

--- discord_bots/the_hand/index.js
+++ discord_bots/the_hand/index.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix, token, giphyToken } = require('./config.json');
 const client = new Discord.Client();
+
+var GphApiClient = require('giphy-js-sdk-core')
+giphy = GphApiClient(giphyToken)
 
 client.once('ready', () => {
     console.log('Ready to serve!');
@@ -11,7 +14,18 @@ client.on('message', message => {
        if(message.content.startsWith(`${prefix}kick`)) {
            let member = message.mentions.members.first();
            member.kick().then((member) => {
-               message.channel.send(`:wave: ${member.displayName} has been kicked.`)
+               giphy.search('gifs', {'q':kicked})
+                   .then((response) => {
+                       var totalResponses = response.data.length;
+                       var responseIndex = Math.floor((math.random() *10) + 1) % totalResponses;
+                       var responseFinal = response.data[responseIndex];
+                       message.channel.send(`:wave: ${member.displayName} has been kicked.`, {
+                           files: [responseFinal.images.fixed_height.url]
+                       })
+                   }).catch(() => {
+                       message.channel.send('Error. Something went wrong')
+               })
+
            })
        }
    }
