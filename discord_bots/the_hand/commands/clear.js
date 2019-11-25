52dfb8add47724b835675d9e32f527b57d2c11c0
Noland Seigler <nolandseigler@gmail.com>
2019-11-24 23:33:10 -0600

    completed th command handling portion of guide.

--- discord_bots/the_hand/commands/clear.js
+++ discord_bots/the_hand/commands/clear.js
@@ -1,12 +1,18 @@
 module.exports = {
     name: 'clear',
-    description: 'clears messages',
+    description: 'clears messages. users must request a value between 1 and 100.',
     execute(message, args) {
-        if (!args.length) {
-            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-        } else if (args[0] === 'foo') {
-            return message.channel.send('bar');
-        } message.channel.send(`First argument: ${args[0]}`);
+        const amount = parseInt(args[0]) + 1;
+
+        if (isNaN(amount)) {
+            return message.reply('that doesn\'t seem to be a valid number.');
+        } else if (amount <= 1 || amount > 100) {
+            return message.reply('you need to input a number between 1 and 99.');
+        }
+        message.channel.bulkDelete(amount, true).catch(err => {
+            console.error(err);
+            message.channel.send('there was an error trying to prune messages in this channel!');
+        });
     },
 };
 
@@ -14,16 +20,16 @@ module.exports = {
 
 
 
-else if (command === 'clear') {
-    const amount = parseInt(args[0]) + 1;
-
-    if (isNaN(amount)) {
-        return message.reply('that doesn\'t seem to be a valid number.');
-    } else if (amount <= 1 || amount > 100) {
-        return message.reply('you need to input a number between 1 and 99.');
-    }
-    message.channel.bulkDelete(amount, true).catch(err => {
-        console.error(err);
-        message.channel.send('there was an error trying to prune messages in this channel!');
-    });
-}
+// else if (command === 'clear') {
+//     const amount = parseInt(args[0]) + 1;
+//
+//     if (isNaN(amount)) {
+//         return message.reply('that doesn\'t seem to be a valid number.');
+//     } else if (amount <= 1 || amount > 100) {
+//         return message.reply('you need to input a number between 1 and 99.');
+//     }
+//     message.channel.bulkDelete(amount, true).catch(err => {
+//         console.error(err);
+//         message.channel.send('there was an error trying to prune messages in this channel!');
+//     });
+// }
--- discord_bots/the_hand/index.js
+++ discord_bots/the_hand/index.js
@@ -1,9 +1,17 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token, giphyToken } = require('./config.json');
-const client = new Discord.Client();
-
 var GphApiClient = require('giphy-js-sdk-core')
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 giphy = GphApiClient(giphyToken)
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log('Ready to serve!');
