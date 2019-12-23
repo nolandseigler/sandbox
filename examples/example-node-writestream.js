4c366ff9ef53f77748e54d7d6edee0504ca9a99f
Noland Seigler <nolandseigler@gmail.com>
2019-12-22 22:30:58 -0600

    read a file by streams then write that file to a new file by stream

---
+++ examples/example-node-writestream.js
@@ -0,0 +1,13 @@
+//read a stream then write a stream
+
+const readline = require('readline');
+const fs = require('fs');
+
+const myInterface = readline.createInterface({
+    input: fs.createReadStream('shoppingList.txt')
+});
+
+const fileStream = fs.createWriteStream('shoppingResults.txt');
+
+const transformData = line => fileStream.write(`They were out of: ${line}\n`);
+myInterface.on('line', transformData);
