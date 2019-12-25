9078ad0ad05bed4e25aa89e4815c442ef902366f
Noland Seigler <nolandseigler@gmail.com>
2019-12-25 17:11:56 -0600

    simple app that might be good for the slack bot

---
+++ examples/express/example-express-simple-get.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Finish this function and use it in the routes below
+
+const sendFruitResponse = (req, res, next) => {
+    res.send('fruit');
+};
+
+app.get('/whatis/apple', sendFruitResponse);
+
+app.get('/whatis/orange', sendFruitResponse);
+
+app.get('/whatis/banana', sendFruitResponse);
+//missing app.listen or server.listen?
