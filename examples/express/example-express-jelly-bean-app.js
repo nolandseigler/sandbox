d30bd7f30a43a3c7c99e0b2f9686c1dc69e1197b
Noland Seigler <nolandseigler@gmail.com>
2019-12-24 21:28:44 -0600

    required body-parser. used it to replace the custom body parser function that i wrote. now it is app.use(bodyParser.json())

--- examples/express/example-express-jelly-bean-app.js
+++ examples/express/example-express-jelly-bean-app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
 
@@ -24,26 +25,14 @@ const jellybeanBag = {
     }
 };
 
-const bodyParser = (req, res, next) => {
-    let queryData = '';
-    req.on('data', (data) => {
-        data = data.toString();
-        queryData += data;
-    });
-    req.on('end', () => {
-        if (queryData) {
-            req.body = JSON.parse(queryData);
-        }
-        next();
-    });
-};
-
 // Logging Middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 app.use('/beans/:beanName', (req, res, next) => {
     const beanName = req.params.beanName;
-    if (!jellybeanBag[beanName]) {return res.status(404).send('Bean with that name does not exist');
+    if (!jellybeanBag[beanName]) {
+        return res.status(404).send('Bean with that name does not exist');
     }
     req.bean = jellybeanBag[beanName];
     req.beanName = beanName;
@@ -54,7 +43,7 @@ app.get('/beans/', (req, res, next) => {
     res.send(jellybeanBag);
 });
 
-app.post('/beans/', bodyParser, (req, res, next) => {
+app.post('/beans/', (req, res, next) => {
     const body = req.body;
     const beanName = body.name;
     if (jellybeanBag[beanName] || jellybeanBag[beanName] === 0) {
@@ -63,20 +52,21 @@ app.post('/beans/', bodyParser, (req, res, next) => {
     const numberOfBeans = Number(body.number) || 0;
     jellybeanBag[beanName] = {
         number: numberOfBeans
-    }; res.send(jellybeanBag[beanName]);
+    };
+    res.send(jellybeanBag[beanName]);
 });
 
 app.get('/beans/:beanName', (req, res, next) => {
     res.send(req.bean);
 });
 
-app.post('/beans/:beanName/add', bodyParser, (req, res, next) => {
+app.post('/beans/:beanName/add', (req, res, next) => {
     const numberOfBeans = Number(req.body.number) || 0;
     req.bean.number += numberOfBeans;
     res.send(req.bean);
 });
 
-app.post('/beans/:beanName/remove', bodyParser, (req, res, next) => {
+app.post('/beans/:beanName/remove', (req, res, next) => {
     const numberOfBeans = Number(req.body.number) || 0;
     if (req.bean.number < numberOfBeans) {
         return res.status(400).send('Not enough beans in the jar to remove!');
