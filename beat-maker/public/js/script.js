8a404789985f54bfcd33f78ecc7fc26a0853e71f
Noland Seigler <nolandseigler@gmail.com>
2019-12-29 21:42:17 -0600

    i followed the first three steps and tested them with the test script. the provided tests from codecademy say I am failing basic functionality however, when i run them in the console they perform as intended. toggleDrum toggles an element of the specified array and the other resets the provided array of bools to false

--- beat-maker/public/js/script.js
+++ beat-maker/public/js/script.js
@@ -1,6 +1,24 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let snares = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let hiHats = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let rideCymbals = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 
+const toggleDrum = (arrName, indexNum) => {
+    if (arrName[indexNum] === false) {
+        arrName[indexNum] = true;
+    } else if (arrName[indexNum] === true) {
+        arrName[indexNum] = false;
+    } else {
+        return console.log("Error in the toggleDrum function");
+    }
+};
+
+const clear = (arrName) => {
+     arrName.fill(false, 0, arrName.length - 1);
+};
+toggleDrum(kicks, 1);
+console.log(kicks);
+console.log(snares);
+clear(kicks);
+console.log(kicks);
