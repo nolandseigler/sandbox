2a0616267e7af374fa525a264e74f46c3fe5e248
Noland Seigler <nolandseigler@gmail.com>
2020-01-01 19:56:55 -0600

    finished handler. testing now

--- beat-maker/presetHandler.js
+++ beat-maker/presetHandler.js
@@ -2,16 +2,24 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = (requestType, arrIndexOfPresets, optionalPutArg) => {
+const presetHandler = (requestType, arrIndexOfPresets, optionalPUTArg) => {
     let outputArr = [];
     if (requestType === "GET" || requestType === "PUT") {
         if (presets[arrIndexOfPresets] === undefined) {
             outputArr.push(404);
         } else if (arrIndexOfPresets !== undefined) {
             outputArr.push(200);
+            if (requestType === "GET") {
+                outputArr.push(presets[arrIndexOfPresets])
+            }
+            if (requestType === "PUT")  {
+                presets[arrIndexOfPresets] = optionalPUTArg;
+                outputArr.push(optionalPUTArg);
+            }
+
         } else {
             console.log("Error in presetHandler");
-        }
+        };
     } else {
         outputArr.push(400);
     }
