9da84b1e898025609459528f31e87a8ddb28df50
Noland Seigler <nolandseigler@gmail.com>
2020-01-01 19:41:07 -0600

    working in presetHandler.js

--- beat-maker/presetHandler.js
+++ beat-maker/presetHandler.js
@@ -2,7 +2,10 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (requestType, arrIndexOfPresets, optionalPutArg) => {
+
+    return outputArr
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
