85261a34605483061fc4b3ffc8e35f43938e0c51
Noland Seigler <nolandseigler@gmail.com>
2020-01-03 20:41:09 -0600

    more practice with for in object loops

--- practice/objects-for-in-loop-practice.js
+++ practice/objects-for-in-loop-practice.js
@@ -25,3 +25,36 @@ let spaceship = {
 for (let crewMember in spaceship.crew) {
     console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
 };
+//iterate through using for in continued
+
+let spaceship = {
+    crew: {
+        captain: {
+            name: 'Lily',
+            degree: 'Computer Engineering',
+            cheerTeam() { console.log('You got this!') }
+        },
+        'chief officer': {
+            name: 'Dan',
+            degree: 'Aerospace Engineering',
+            agree() { console.log('I agree, captain!') }
+        },
+        medic: {
+            name: 'Clementine',
+            degree: 'Physics',
+            announce() { console.log(`Jets on!`) } },
+        translator: {
+            name: 'Shauna',
+            degree: 'Conservation Science',
+            powerFuel() { console.log('The tank is full!') }
+        }
+    }
+};
+
+for (let obj in spaceship['crew']) {
+    console.log(`${obj}: ${spaceship['crew'][obj]['name']}`);
+};
+
+for (let obj in spaceship['crew']) {
+    console.log(`${spaceship['crew'][obj]['name']}: ${spaceship['crew'][obj]['degree']}`);
+};
