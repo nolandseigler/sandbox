c5b5e152bda00ffe9bf79ad84133589306b885e9
Noland Seigler <nolandseigler@gmail.com>
2020-01-03 22:06:57 -0600

    setter method

--- practice/object-bracket-notation-practice.js
+++ practice/object-bracket-notation-practice.js
@@ -87,4 +87,26 @@ const robot = {
 };
 
 console.log(robot.energyLevel);
+//setter method added. notice how the value is set by calling the function as if you were just assigning the value to a property
+const robot = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    _numOfSensors: 15,
+    get numOfSensors(){
+        if(typeof this._numOfSensors === 'number'){
+            return this._numOfSensors;
+        } else {
+            return 'Sensors are currently down.'
+        }
+    },
+    set numOfSensors(num) {
+        if (typeof num === 'number' && num >= 0) {
+            this._numOfSensors = num;
+        } else {
+            console.log('Pass in a number that is greater than or equal to 0');
+        };
+    }
+};
 
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors);
